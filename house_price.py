c18acbb521edce3abdb36b8628ad87a51c0e16d4
Cobra30621 <cobra20621@gmail.com>
2022-06-14 23:42:45 +0800

    fix: 畫面顯示

--- house_price.py
+++ house_price.py
@@ -22,11 +22,11 @@ st.subheader("1.房屋面積")
 area_col1, area_col2, area_col3 = st.columns(3)
 
 with area_col1:
-    Transfer_Total_Ping = st.number_input('轉移坪數', value=20 )
+    Transfer_Total_Ping = st.number_input('轉移坪數', value=20.0 )
 with area_col2:
-    main_area = st.number_input('主建物面積', value=10 )
+    main_area = st.number_input('主建物面積', value=10.0 )
 with area_col3:
-    area_ping = st.number_input('土地轉移面積', value=5 )
+    area_ping = st.number_input('土地轉移面積', value=5.0 )
 
 st.subheader("2.房廳衛")
 hrb_col1, hrb_col2, hrb_col3 = st.columns(3)
@@ -43,9 +43,9 @@ st.subheader("3.房子高度")
 
 build_col1, build_col2 = st.columns(2)
 with build_col1:
-    building_total_floors = st.number_input('建築總高度', step=1, value=1 )
+    building_total_floors = st.number_input('總樓層數', step=1, value=1 )
 with build_col2:
-    min_floors_height = st.number_input('建築樓層', step=1, value=1 )
+    min_floors_height = st.number_input('交易樓層', step=1, value=1 )
 
 
 st.subheader("4.其他")
@@ -61,7 +61,7 @@ with other_col2:
 
 
 
-st.subheader("二、房價預測")
+st.header("二、房價預測")
 
 
 kwargs = { "Transfer_Total_Ping" : Transfer_Total_Ping, "main_area": main_area,
@@ -77,8 +77,8 @@ unit_price = 0
 if(Transfer_Total_Ping != 0):
     unit_price = format(round(Total_price / Transfer_Total_Ping), ',d')
 
-st.markdown('#### 房價 : {}'.format(house_price))
-st.markdown('#### 單位面積 : {}'.format(unit_price))
+st.markdown('#### 總房價 : {}'.format(house_price))
+st.markdown('#### 單價元平方公尺 : {}'.format(unit_price))
 st.info("計算方式: 總房價 / 轉移面積")
 
 
