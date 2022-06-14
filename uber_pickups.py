7a478d34fcb2d3c21f9f0c1eb60ad71cc57045c6
Cobra30621 <cobra20621@gmail.com>
2022-06-14 21:16:38 +0800

    add house_price to uber_pickups

--- uber_pickups.py
+++ uber_pickups.py
@@ -2,6 +2,16 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
+from model import ModelManager
+
+
+
+# 事前讀取
+place_df = pd.read_csv('place_id.csv')
+
+
+
+model = ModelManager()
 
 
 st.title('台灣房價預測網站')
@@ -12,68 +22,56 @@ st.subheader("1.房屋面積")
 area_col1, area_col2, area_col3 = st.columns(3)
 
 with area_col1:
-    Transfer_Total_Ping = st.number_input('轉移坪數', step=1)
+    Transfer_Total_Ping = st.number_input('轉移坪數', value=20 )
 with area_col2:
-    main_area = st.number_input('主建物面積', step=1)
+    main_area = st.number_input('主建物面積', value=10 )
 with area_col3:
-    area_ping = st.number_input('土地轉移面積', step=1)
+    area_ping = st.number_input('土地轉移面積', value=5 )
 
 st.subheader("2.房廳衛")
 hrb_col1, hrb_col2, hrb_col3 = st.columns(3)
 
 with hrb_col1:
-    room = st.slider('房', 0, 5, 2)
+    room = st.number_input('房', step=1, value=1 ) 
 with hrb_col2:
-    hall = st.slider('廳', 0, 5, 2)
+    hall = st.number_input('廳', step=1, value=1 )
 with hrb_col3:
-    bathroom = st.slider('衛', 0, 5, 2)
+    bathroom = st.number_input('衛', step=1, value=1 ) 
 
-st.subheader("3.其他")
-
-house_age = st.number_input('屋齡')
-
-Total_price = '100,000,000'
-unit_price = '1,000,000'
-if st.button('預測房價'):
-    st.subheader("二、房價預測")
-    st.success('房價 : {}'.format(Total_price))
-    st.success('每坪價格 : {}'.format(unit_price))
-else:
-    st.write('')
+st.subheader("3.房子高度")
 
 
-df = pd.DataFrame(
-    np.random.randn(1000, 2) / [25, 25] + [22.7, 120.4],
-    columns=['lat', 'lon'])
+build_col1, build_col2 = st.columns(2)
+with build_col1:
+    building_total_floors = st.number_input('建築物多高', step=1, value=1 )
+with build_col2:
+    min_floors_height = st.number_input('建築物幾樓', step=1, value=1 )
 
-st.header("三、地圖區間")
-st.write("可以使用pydeck顯示地圖資料，不過需要經緯度")
 
-st.pydeck_chart(pdk.Deck(
-     map_style='mapbox://styles/mapbox/light-v9',
-     initial_view_state=pdk.ViewState(
-         latitude=22.6,
-         longitude=120.4,
-         zoom=10,
-         pitch=50,
-     ),
-     layers=[
-         pdk.Layer(
-            'HexagonLayer',
-            data=df,
-            get_position='[lon, lat]',
-            radius=200,
-            elevation_scale=4,
-            elevation_range=[0, 1000],
-            pickable=True,
-            extruded=True,
-         ),
-         pdk.Layer(
-             'ScatterplotLayer',
-             data=df,
-             get_position='[lon, lat]',
-             get_color='[200, 30, 0, 160]',
-             get_radius=200,
-         ),
-     ],
- ))
+st.subheader("4.其他")
+other_col1, other_col2 = st.columns(2)
+with other_col1:
+    house_age = st.number_input('屋齡')
+with other_col2:
+    option = st.selectbox(
+     '市區',
+    place_df['place']  )
+    place_id = place_df[place_df['place'] == option].reset_index()['place_id'][0]
+
+
+
+
+st.subheader("二、房價預測")
+
+
+kwargs = { "Transfer_Total_Ping" : Transfer_Total_Ping, "main_area": main_area,
+    "area_ping" : area_ping,
+    "room" : room, "hall" : hall, "bathroom" : bathroom,
+    "building_total_floors" : building_total_floors, "min_floors_height": min_floors_height,
+    "house_age": house_age, "Place_id" : place_id}
+
+Total_price = model.predict(**kwargs)
+house_price = format(round(Total_price), ',d')
+
+st.markdown('### 房價 : {}'.format(house_price))
+
